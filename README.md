f59481a2313a817c5c63b0a9995b5edc24a9a29a
itzo <me@itzo.org>
2016-03-18 01:37:20 -0400

    updated readme.

--- README.md
+++ README.md
@@ -26,3 +26,44 @@ usage: ./bittrex.py [-iph] -m market
      -m [--market]   specifies the market to use
      -h [--help]     prints this menu
 ```
+
+## driveshare-check.py
+Check the status of a payout address against Storj's driveshare API
+
+examples:
+
+Using a password file with no prompts. This is useful for automated processes such as adding a cronjob to run this check and email you occasionally. TODO: alert if instance goes down, otherwise email status report only once a week.
+```
+$ ./driveshare-check.py me@itzo.org -a <payout address> -u <gmail user> -p <secret file>
+Message sent to '['me@itzo.org']'.
+```
+Interactive run with no arguments passed on the command line
+```
+$ ./driveshare-check.py me@itzo.org
+Gmail username: <gmail user>
+Password: <gmail password>
+Payout address: <payout address>
+Message sent to '['me@itzo.org']'.
+```
+Detailed help menu
+```
+$ ./driveshare-check.py -h
+usage: driveshare-check.py [-h] [-u USER] [-p PWFILE] [-a ADDRESS] [-d]
+                           recipients [recipients ...]
+
+positional arguments:
+  recipients            The recipient email addresses (space delimited).
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -u USER, --user USER  The sender's Gmail username or email address. The
+                        username@gmail.com will be used as the Friendly From
+                        header of the email.
+  -p PWFILE, --pwfile PWFILE
+                        File containing the password for Gmail username
+                        (optional). If a file is not provided the user will be
+                        asked to enter a password.
+  -a ADDRESS, --address ADDRESS
+                        The user's SJCX payout address to check the status of.
+  -d, --debug           Enable debugging for the SMTP server interaction.
+```
---
+++ driveshare-check.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+# author:       itzo <me@itzo.org>
+# version:      1.0
+# description:  Check the status of a payout address against the Driveshare API data.
+#               Send an email with the current status of the instance(s). 
+# comments:     Less Secure Apps must be enabled in the sender's Google account.
+#               See: https://www.google.com/settings/security/lesssecureapps
+
+import urllib
+import json
+import smtplib
+import argparse
+from getpass import getpass
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('recipients',
+        help='The recipient email addresses (space delimited).',
+        nargs='+'
+    )
+    parser.add_argument('-u', '--user',
+        help='The sender\'s Gmail username or email address. The username@gmail.com will be used as the Friendly From header of the email.',
+    )
+    parser.add_argument('-p', '--pwfile',
+        help='File containing the password for Gmail username (optional). If a file is not provided the user will be asked to enter a password.',
+    )
+    parser.add_argument('-a', '--address',
+        help='The user\'s SJCX payout address to check the status of.',
+    )
+    parser.add_argument('-d', '--debug',
+        action='store_true',
+        help='Enable debugging for the SMTP server interaction.'
+    )
+
+    return parser
+
+
+# parse arguments
+def parse_args():
+    parser = get_parser()
+    args = parser.parse_args()
+    # gmail user/pw and payout address are required
+    if args.user is None:
+        args.user = raw_input('Gmail username: ')
+    if args.pwfile is None:
+        args.pwfile = getpass()
+    else:
+        pwfile = open(args.pwfile, 'r')
+        args.pwfile = file.read(pwfile)
+    if args.address is None:
+        args.address = raw_input('Payout address: ')
+    return args
+
+
+# create the actual email message
+def create_message(args,body):
+    FROM    = args.user
+    TO      = ','.join(args.recipients)
+    SUBJECT = "Driveshare Status Report"
+    TEXT    = body
+    msg     = """From: %s\nTo: %s\nSubject: %s\n\n%s""" % (FROM, TO, SUBJECT, TEXT)
+    return msg
+
+
+# main function
+def main():
+    args = parse_args()
+    payout_addr = args.address
+    url = 'http://status.driveshare.org/api/online/json'
+    password = args.pwfile
+
+    # capture the data into an object
+    json_obj = urllib.urlopen(url)
+    data = json.load(json_obj)
+
+    # grab our payout address
+    count = 0
+    body = ""
+    for item in data['farmers']:
+        if item['payout_addr'] == payout_addr:
+            body += str(item['btc_addr'])+" "+str(item['height'])+" "+str(item['last_seen'])+"sec\n"
+            count += 1
+    body += "\n"+str(count)+" instances found."
+    msg = create_message(args,body)
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.set_debuglevel(args.debug)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(args.user, password)
+        server.sendmail(args.user, args.recipients, msg)
+        print "Message sent to '%s'." % args.recipients
+        server.close()
+    except smtplib.SMTPAuthenticationError as e:
+        print "Unable to send message: %s" % e
+
+if __name__ == "__main__":
+    main()
