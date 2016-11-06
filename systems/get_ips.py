88953ab550b8129beb13da8789f8ca4da14a1dcf
itzo <me@itzo.org>
2016-11-06 00:27:01 -0400

    initial commit

--- systems/get_ips.py
+++ systems/get_ips.py
@@ -1,39 +1,21 @@
 #!/usr/bin/env python
+# get all ip's and networks in environment
+# ./get_ips.py hosts
+
+# TODO: switch to paramiko and make multithreaded
+# TODO: collect more information, check for duplicate IP's, wrong configurations, gateways, routing, etc..
+# TODO: add usage, input validation
 
 import subprocess
 import sys
 
-d = {}
+with open(sys.argv[1], 'r') as hostlist:
+    for host in hostlist:
+        host = host.strip()
 
-for host in sys.stdin:
-    host = host.strip()
-    iface = subprocess.Popen("ssh root@" +host+ " 'ip -4 addr | grep inet | grep -v 127.0.0.1 | cut -d\" \" -f6'", stdout=subprocess.PIPE, shell=True).communicate()[0].split()
+        iface = subprocess.Popen("ssh -o ConnectTimeout=3 root@" + host +
+                " 'ip -4 addr | grep inet | grep -v 127.0.0.1 | cut -d\" \" -f6'",
+                stdout=subprocess.PIPE, shell=True).communicate()[0].split()
 
-    # logic to get only the lowest ip in each subnet
-    for i in range(len(iface)-1):
-        try:
-            t = iface[i+1]
-        except IndexError:
-            continue
-        a = iface[i].split('/')
-        b = iface[i+1].split('/')
-        temp_a = a[0].split('.')
-        temp_b = b[0].split('.')
-        # compare elements and delete the larger from the list
-        if str(temp_a[0]+temp_a[1]+temp_a[2]) == str(temp_b[0]+temp_b[1]+temp_b[2]):
-            if temp_a[3] < temp_b[3]:
-                del iface[i+1]
-            else:
-                del iface[i]
-
-    for i in iface:
-        d[i] = i[:-3]
-
-# print out our IP's
-# sorry once again for ugliness below. noticed I wasn't printing the output correctly
-# with about 10 minutes left...
-for key in d:
-    print ( key.split('/')[0].split('.')[0] + '.' +
-            key.split('/')[0].split('.')[1] + '.' +
-            key.split('/')[0].split('.')[2] + '.' +
-            "0/" + key.split('/')[1] + " - " + d[key])
+        for i in iface:
+            print ("%-17s %s" % (i.split('/')[0].rsplit('.',1)[0]+'.0/'+i.split('/')[1], i))
